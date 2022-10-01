baf13d40abd7987716801787bc4fa536594f4739
Pedro Souza <pedroisidorio1@gmail.com>
2022-10-01 18:03:25 -0300

    apagando dados guardados

---
+++ components/footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }} Direitos reservados para Pedro Isidorio</span>
+    </v-footer>
+</template>
